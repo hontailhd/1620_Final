30baed6f0e05153b25a0bddb0f334361de3585f6
James Kim <hontailhd@gmail.com>
2021-04-21 14:17:28 -0700

    no time to finish js time to hand it in

---
+++ Scripts/apps.js
@@ -0,0 +1,16 @@
+const albums = [
+    { name: "Album 1", price: 12.99, inCart: false }, 
+    { name: "Album 2", price: 14.99, inCart: false }, 
+    { name: "Album 3", price: 9.99, inCart: false }, 
+    { name: "Album 4", price: 19.99, inCart: false }
+  ];
+
+  //update the src for img
+  function imgPrep(album) {
+    for i in album {
+        i
+    } 
+
+ 
+
+  imgPrep(album)
